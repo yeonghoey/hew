a449b09d2417010e573a729c9bc22094a4f331b7
Yeongho Kim <yeonghoey@gmail.com>
2018-02-08 07:26:28 +0900

    Specify click.option types to directory path

--- hew/__main__.py
+++ hew/__main__.py
@@ -10,13 +10,16 @@ import hew.vlc
 from hew.util import Scheme
 
 
+DIR = click.Path(exists=True, file_okay=False, dir_okay=True)
+
+
 @click.command()
-@click.option('--anki-media', envvar='ANKI_MEDIA')
+@click.option('--anki-media', type=DIR, envvar='ANKI_MEDIA')
 @click.option('--yt', is_flag=True)
 @click.option('--yt-itag', default=18, type=int, help='18: 360p, 22: 720p,...')
 @click.option('--yt-lang', default='en', help='for caption, such as "en"')
 @click.option('--convert-wav', is_flag=True)
-@click.option('--snapshot-dir')
+@click.option('--snapshot-dir', type=DIR)
 @click.option('--srt-padding', type=int, default=2000)
 @click.argument('source')
 @click.argument('start-at', default=None, required=False)
